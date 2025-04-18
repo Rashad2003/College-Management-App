54fec6f87f5c8185da83ec4045448e34032c9e79
Mohammed Rashad <125732535+Rashad2003@users.noreply.github.com>
2025-04-18 23:38:18 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext();
 
 const StoreContextProvider = ({ children }) => {
   const [token, setToken] = useState("");
-  const backendUrl = "http://localhost:5000";
+  const backendUrl = "https://student-attendance-management-backend-00ey.onrender.com/";
   useEffect(() => {
     if (!token && localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
