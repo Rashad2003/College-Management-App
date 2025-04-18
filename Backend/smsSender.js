b83eb18db37b2af579dd1d0ae7773e1dc04cee91
Rashad2003 <125732535+Rashad2003@users.noreply.github.com>
2025-04-18 09:33:12 +0530

    second commit

--- Backend/smsSender.js
+++ Backend/smsSender.js
@@ -1,8 +1,8 @@
 import twilio from "twilio";
 
-const accountSid = "AC074dd3572aaa5244becad17820a03489";
-const authToken = "f76a9af3a5d73f8139b5510f436db777";
-const fromNumber = +19786449234;
+const accountSid = process.env.TWILIO_ACCOUNT_SID || "AC074dd3572aaa5244becad17820a03489";
+const authToken = process.env.TWILIO_AUTH_TOKEN || "f76a9af3a5d73f8139b5510f436db777";
+const fromNumber = process.env.TWILIO_PHONE_NUMBER || +19786449234;
 
 const client = twilio(accountSid, authToken);
 
