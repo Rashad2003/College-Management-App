3172e12a17d4ce01be58eb2ec2f09b759e93da18
Mohammed Rashad <125732535+Rashad2003@users.noreply.github.com>
2025-04-20 08:40:37 +0530

    Update userRoute.js

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, listUser, loginUser, registerUser, updateUser } from "../controllers/userController.js";
+import { deleteUser, listUser, loginUser, registerUser, updateUser, forgotPassword } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post("/register", registerUser);
 router.get("/list", listUser);
 router.put("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
-
+router.post("/forgot-password", forgotPassword);
 router.post("/login", loginUser);
 
 export default router;
