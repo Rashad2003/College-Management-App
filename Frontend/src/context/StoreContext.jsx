69538361ee42fef78a8d433ba352e0bcae526562
Mohammed Rashad <125732535+Rashad2003@users.noreply.github.com>
2025-05-02 14:07:08 +0530

    Update StoreContext.jsx ------\

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext();
 
 const StoreContextProvider = ({ children }) => {
   const [token, setToken] = useState("");
-  const backendUrl = "http://localhost:5000";
+  const backendUrl = "https://student-attendance-management-six.vercel.app";
   useEffect(() => {
     if (!token && localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
